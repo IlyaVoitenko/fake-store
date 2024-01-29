c69f13fcf899ac38ff93a3b6b678b11d05356286
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2024-01-29 14:32:49 +0100

    changed page not found

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -3,12 +3,23 @@ import { useNavigate } from "react-router-dom";
 const PageNotFound = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-row justify-center items-center w-screen h-screen ">
-      <span>Page not found :</span>
-      <button className="ml-2" onClick={() => navigate(`/`)}>
-        go home
+    <main className="h-screen w-screen flex flex-col justify-center items-center bg-[#1A2238]">
+      <h1 className="text-9xl font-extrabold text-white tracking-widest">
+        404
+      </h1>
+      <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+        Page Not Found
+      </div>
+      <button className="mt-5">
+        <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
+          <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+          <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+            <button onClick={() => navigate("/")}>Go Home</button>
+          </span>
+        </a>
       </button>
-    </div>
+    </main>
   );
 };
 
